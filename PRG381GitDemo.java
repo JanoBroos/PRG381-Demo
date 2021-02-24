512f6ff03b51e1b6c6c583c46d1a58a11511e660
Name & Surname <jano.broos@netactive.co.za>
2021-02-24 11:05:04 +0200

    Killing some branches

--- PRG381GitDemo.java
+++ PRG381GitDemo.java
@@ -4,6 +4,10 @@ public class PRG381GitDemo {
     public static void main(String[] args) {
         System.out.println("Git Boi");
 
-        //Quick Push
+        Murder();
+    }
+
+    void MURDER(){
+        //Kill
     }
 }
